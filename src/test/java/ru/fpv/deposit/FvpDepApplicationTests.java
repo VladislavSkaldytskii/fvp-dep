cb5eb7cba1328a1a0185b8b1efac8fcc3359bc7b
Vladislav Skaldytskiy <Svetovitd@yandex.ru>
2026-02-19 17:14:13 +0300

    FPV-1: create application ,application configuration

---
+++ src/test/java/ru/fpv/deposit/FvpDepApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fpv.deposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FvpDepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
